af1655be4cf701a928a850778e0777d4d63f8c9d
ChristAren <76263250+ChristAren@users.noreply.github.com>
2021-01-18 21:25:21 -0500

    Needs more custom

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+document.getElementById("Submit").addEventListener("click", monthlyPay)
+
+let Input = parseInt(document.getElementById("loanMonths"))
+let Input2 = parseInt(document.getElementById("loanAmount"))
+let Input3 = parseInt(document.getElementById("interestRate"))
+
+function monthlyPay() {
+    for(let loop=0; loop < Input; loop++);
+    
+
+
+
+}
+
+    (Input2) * (Input3/1200) / (1 - (1 + Input3/1200)
+(-Input)
+before the very first month equals the amount of the loan.
+Previous Remaining Balance * rate/1200
+Total Monthly Payment - Interest Payment
+Previous Remaining Balance - principal payments
+);
+outputArea1.innerHTML = textOutput
+
+
